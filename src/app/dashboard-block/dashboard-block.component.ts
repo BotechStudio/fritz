bed5dd5520798bb57ecfebcdf768640e819decc4
Yaroslav Suntsov <yaroslav.suntsov@dev-optima.com>
2025-01-10 00:08:17 +0200

    Adjust styles add new icons.

--- src/app/dashboard-block/dashboard-block.component.ts
+++ src/app/dashboard-block/dashboard-block.component.ts
@@ -1,28 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+interface MenuItem {
+  label: string;
+  img: string;
+  option: string[];
+}
 
 @Component({
   selector: 'app-dashboard-block',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, SvgIconComponent],
   templateUrl: './dashboard-block.component.html',
   styleUrls: ['./dashboard-block.component.css'],
 })
 export class DashboardBlockComponent {
-  menuItems = [
-    { label: 'משימות', icon: 'task', option: ['hub'] },
-    { label: 'הזמנות', icon: 'shopping_cart', option: ['plane'] },
-    { label: 'מעקב משלוחים', icon: 'local_shipping', option: ['hub'] },
-    { label: 'בקרת איכות', icon: 'checklist', option: ['hub'] },
-    { label: 'ניהול לקוחות', icon: 'group', option: ['plane', 'hub'] },
-    { label: 'ניהול משתמשים', icon: 'account_circle', option: ['hub'] },
-    { label: 'דוחות', icon: 'insert_chart', option: [] },
-    { label: 'ארכיב', icon: 'archive', option: ['hub', 'plane'] },
-    { label: 'פיננסי', icon: 'attach_money', option: [] },
-    { label: 'BOOKING', icon: 'settings', option: ['plane'] },
+  menuItems: MenuItem[] = [
+    { label: 'משימות', img: 'assets/images/item-img1.svg', option: ['hub'] },
+    { label: 'הזמנות', img: 'assets/images/item-img2.svg', option: ['plane'] },
+    { label: 'מעקב משלוחים', img: 'assets/images/item-img3.svg', option: ['hub'] },
+    { label: 'בקרת איכות', img: 'assets/images/item-img4.svg', option: ['hub'] },
+    { label: 'ניהול לקוחות', img: 'assets/images/item-img5.svg', option: ['plane', 'hub'] },
+    { label: 'ניהול משתמשים', img: 'assets/images/item-img6.svg', option: ['hub'] },
+    { label: 'דוחות', img: 'assets/images/item-img7.svg', option: [] },
+    { label: 'ארכיב', img: 'assets/images/item-img8.svg', option: ['hub', 'plane'] },
+    { label: 'פיננסי', img: 'assets/images/item-img9.svg', option: [] },
+    { label: 'BOOKING', img: 'assets/images/item-img10.svg', option: ['plane'] },
   ];
-  selectedItem = null;
+
+  selectedItem: MenuItem | null = null;
+
 
   onSelect(item: any) {
     this.selectedItem = item;
