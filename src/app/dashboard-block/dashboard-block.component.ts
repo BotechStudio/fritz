41b7b5e2efe59fcc208ea34e3315b3335b3ff633
Yaroslav Suntsov <yaroslav.suntsov@dev-optima.com>
2025-01-14 11:58:51 +0200

    Adjust styles, texts, icons.

--- src/app/dashboard-block/dashboard-block.component.ts
+++ src/app/dashboard-block/dashboard-block.component.ts
@@ -18,16 +18,17 @@ interface MenuItem {
 })
 export class DashboardBlockComponent {
   menuItems: MenuItem[] = [
-    { label: 'משימות', img: 'assets/images/item-img1.svg', option: ['hub'] },
-    { label: 'הזמנות', img: 'assets/images/item-img2.svg', option: ['plane'] },
-    { label: 'מעקב משלוחים', img: 'assets/images/item-img11.svg', option: ['hub'] },
     { label: 'בקרת איכות', img: 'assets/images/item-img10.svg', option: [] },
-    { label: 'ניהול לקוחות', img: 'assets/images/item-img5.svg', option: ['plane', 'hub'] },
-    { label: 'ניהול משתמשים', img: 'assets/images/item-img6.svg', option: ['hub'] },
-    { label: 'דוחות', img: 'assets/images/item-img7.svg', option: [] },
-    { label: 'ארכיב', img: 'assets/images/item-img8.svg', option: ['hub', 'plane'] },
-    { label: 'פיננסי', img: 'assets/images/item-img9.svg', option: [] },
-    { label: 'BOOKING', img: 'assets/images/item-img10.svg', option: ['plane'] },
+    { label: 'מעקב משלוחים', img: 'assets/images/item-img11.svg', option: ['plane'] },
+    { label: 'הזמנות', img: 'assets/images/item-img2.svg', option: ['hub'] },
+    { label: 'משימות', img: 'assets/images/item-img1.svg', option: ['hub'] },
+    { label: 'ארכיב', img: 'assets/images/item-img6.svg', option: [''] },
+    { label: 'דוחות', img: 'assets/images/item-img5.svg', option: ['plane', 'hub'] },
+    { label: 'ניהול משתמשים', img: 'assets/images/item-img4.svg', option: ['hub'] },
+    { label: 'ניהול לקוחות', img: 'assets/images/item-img3.svg', option: ['hub'] },
+    { label: 'ניהול', img: 'assets/images/item-img9.svg', option: ['plane'] },
+    { label: 'פיננסים', img: 'assets/images/item-img8.svg', option: [] },
+    { label: 'בקרת איכות', img: 'assets/images/item-img7.svg', option: [] },
   ];
 
   selectedItem: MenuItem | null = null;
